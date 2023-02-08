f32300a61dc38e6c25a70974c5e1e0adf56ea6d6
Kelsey Florek <kelseyflorek22@gmail.com>
2023-02-08 11:36:32 -0600

    updated validation for stochastic assembly [test]

--- bin/workflow_validation.py
+++ bin/workflow_validation.py
@@ -18,10 +18,26 @@ test_results = pd.read_csv(os.path.abspath(args.spntypeid_report_test),sep=',',i
 
 ### Validate Results
 validation = valid_results.compare(test_results,align_axis=0,result_names=("Valid Data","Test Data"))
+
+### If no difference validation is successful
+if validation.empty:
+    print("Validation check Successful!")
+    sys.exit()
+
+### If assembly length differs by less than 100 bp then remove from dataframe
+if "Assembly Length (bp)" in validation.columns:
+    for sample in validation["Assembly Length (bp)"].index.get_level_values('Sample').unique():
+        valid_data = validation["Assembly Length (bp)"].loc[sample,"Valid Data"]
+        test_data = validation["Assembly Length (bp)"].loc[sample,"Test Data"]
+        diff = abs(valid_data-test_data)
+        if diff < 100:
+            validation = validation.drop(sample,axis=0,level='Sample')
+
+### If no difference validation is successful
 if validation.empty:
     print("Validation check Successful!")
     sys.exit()
 else:
-    print("Failed Validation Check")
+    print("Validation Failure")
     print(validation)
     sys.exit(1)
