20af180ac794cbc6310a80d8811ecf00435c715d
Eva Gunawan <gunawanevaa@gmail.com>
2025-04-30 16:09:40 -0500

    Fixed quality stats module and script to be functional from bin folder

--- bin/quality_stats.py
+++ bin/quality_stats.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3.7
-import glob
 import os
+import sys
+import glob
+import argparse
+
 from numpy import median
 from numpy import average
 
+def parse_args(args=None):
+	Description='A script to summarize stats'
+	Epilog='Use with quality_stats.py <MINAVGREADQ>'
+
+	parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
+	parser.add_argument('minavgreadq',
+	    help='This is supplied by the nextflow config and can be changed via the usual methods i.e. command line.')
+	return parser.parse_args(args)
+
 # function for summarizing samtools depth files
-def summarize_qual(file):
+def summarize_qual(file, minavgreadq):
     # get sample id from file name and set up data list
     sid = os.path.basename(file).split('.')[0]
     data = []
@@ -17,20 +29,27 @@ def summarize_qual(file):
     med = int(float(median(data)))
     avg = int(float(average(data)))
     # return sample id, median and average depth, and check for coverage fail
-    if avg >= int(${params.minavgreadq}):
+    if avg >= int(minavgreadq):
         result = f"{sid}\\t{med}\\t{avg}\\tTRUE\\t\\n"
-    if avg < int(${params.minavgreadq}):
-        result = f"{sid}\\t{med}\\t{avg}\\tFALSE\\tAverage read quality < ${params.minavgreadq}\\n"
+    if avg < int(minavgreadq):
+        result = f"{sid}\\t{med}\\t{avg}\\tFALSE\\tAverage read quality < minavgreadq\\n"
     return result
 
-# get all bioawk quality files
-files = glob.glob("data*/*.qual.tsv")
 
-# summarize read quality
-results = map(summarize_qual,files)
+def main(args=None):
+    args = parse_args(args)
 
-# write results to file
-with open('quality_stats.tsv', 'w') as outFile:
-    outFile.write("Sample\\tMedian Read Quality\\tAverage Read Quality\\tPass Average Read Quality\\tComments\\n")
-    for result in results:
-        outFile.write(result)
+    # get all bioawk quality files
+    files = glob.glob("data*/*.qual.tsv")
+
+    # summarize read quality
+    results = map(summarize_qual,files, args.minavgreadq)
+
+    # write results to file
+    with open('quality_stats.tsv', 'w') as outFile:
+        outFile.write("Sample\\tMedian Read Quality\\tAverage Read Quality\\tPass Average Read Quality\\tComments\\n")
+        for result in results:
+            outFile.write(result)
+
+if __name__ == "__main__":
+    sys.exit(main())
