282f0a909f952ff8c2e492992eefe5f9026cfe75
AbigailShockey <abbashockey@gmail.com>
2025-06-02 12:16:55 -0500

    Update python version in script

--- bin/assembly_stats_summary.py
+++ bin/assembly_stats_summary.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.7
 import os
 import glob
 import logging
--- bin/kraken_summary.py
+++ bin/kraken_summary.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3.7
 import os
 import glob
 import logging
