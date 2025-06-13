a14c48e760475952b381fe7160c16756f6945167
evagunawan <gunawanevaa@gmail.com>
2025-06-13 16:01:36 -0500

    Removed static values

--- bin/assembly_stats_summary.py
+++ bin/assembly_stats_summary.py
@@ -19,16 +19,16 @@ def summarize_assembly_file(file):
     df = pd.read_csv(file, sep='\t')
 
     logging.debug("Get expected, actual length, ratio, and z score columns")
-    df = df.loc[:,['Expected_length','Actual_length','Ratio Actual:Expected','Z_score', 'Stdev']]
+    df = df.loc[:,['Actual_length','Ratio Actual:Expected','Z_score']]
 
     logging.debug("Assign sample id as column")
     df = df.assign(Sample=sample_id)
 
     logging.debug("Rename columns")
-    df = df.rename(columns={'Expected_length':'Expected length','Actual_length':'Actual length','Ratio Actual:Expected':'Ratio of Actual:Expected Genome length','Z_score':'Z score'})
+    df = df.rename(columns={'Actual_length':'Actual length','Ratio Actual:Expected':'Ratio of Actual:Expected Genome length','Z_score':'Z score'})
 
     logging.debug("Re-order data frame")
-    df = df[['Sample','Expected length','Actual length','Ratio of Actual:Expected Genome length','Z score', 'Stdev']]
+    df = df[['Sample','Actual length','Ratio of Actual:Expected Genome length','Z score']]
 
     return df
 
